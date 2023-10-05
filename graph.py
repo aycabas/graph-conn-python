19fea3b57f7c0ebf82f90294c2cbd2239e5f7954
Ayca Bas <36196437+aycabas@users.noreply.github.com>
2023-10-05 18:01:49 +0400

    update

--- graph.py
+++ graph.py
@@ -14,6 +14,7 @@ from msgraph.generated.models.email_address import EmailAddress
 from msgraph.generated.models.external_connectors.external_connection import ExternalConnection
 from msgraph.generated.models.external_connectors.schema  import Schema
 from msgraph.generated.models.external_connectors.property_  import Property_
+from msgraph.generated.models.external_connectors.property_type import PropertyType
 from kiota_abstractions.method import Method
 from msgraph.graph_request_adapter import GraphRequestAdapter
 import json
@@ -31,7 +32,7 @@ class Graph:
 
         self.device_code_credential = DeviceCodeCredential(client_id, tenant_id = tenant_id)
         self.user_client = GraphServiceClient(self.device_code_credential, graph_scopes)
-
+   
     async def get_user_token(self):
         graph_scopes = self.settings['graphUserScopes']
         access_token = self.device_code_credential.get_token(graph_scopes)
@@ -50,43 +51,6 @@ class Graph:
         user = await self.user_client.me.get(request_configuration=request_config)
         return user
     
-    async def get_inbox(self):
-        query_params = MessagesRequestBuilder.MessagesRequestBuilderGetQueryParameters(
-            # Only request specific properties
-            select=['from', 'isRead', 'receivedDateTime', 'subject'],
-            # Get at most 25 results
-            top=25,
-            # Sort by received time, newest first
-            orderby=['receivedDateTime DESC']
-        )
-        request_config = MessagesRequestBuilder.MessagesRequestBuilderGetRequestConfiguration(
-            query_parameters= query_params
-        )
-
-        messages = await self.user_client.me.mail_folders.by_mail_folder_id('inbox').messages.get(
-                request_configuration=request_config)
-        return messages
-    
-    async def send_mail(self, subject: str, body: str, recipient: str):
-        message = Message()
-        message.subject = subject
-
-        message.body = ItemBody()
-        message.body.content_type = BodyType.Text
-        message.body.content = body
-
-        to_recipient = Recipient()
-        to_recipient.email_address = EmailAddress()
-        to_recipient.email_address.address = recipient
-        message.to_recipients = []
-        message.to_recipients.append(to_recipient)
-
-        request_body = SendMailPostRequestBody()
-        request_body.message = message
-
-        await self.user_client.me.send_mail.post(body=request_body)
-
-
     async def create_connection(self):
         # create prompt for user to enter connection details
         connectionId = input("Enter Connection Id: ")
@@ -99,47 +63,39 @@ class Graph:
             description=connectionDescription,
         )
 
+        print("Creating connection...")
         connection = await self.user_client.external.connections.post(body=new_connection)
         print ("Connection name:", connection.name)
 
     async def get_connection(self, connectionId: str):
+        print("Getting connection...")
         connection= await self.user_client.external.connections.by_external_connection_id(connectionId).get()
         print ("Connection name:", connection.name)
 
     async def list_connections(self):
+        print("Getting connections...")
         connections = await self.user_client.external.connections.get()
         for connection in connections.value:
             print("Connection name:", connection.name)
 
     async def delete_connection(self, connectionId: str):
         connectionId = input("Enter Connection Id to Delete: ")
+        print("Deleting connection...")
         connection = await self.user_client.external.connections.by_external_connection_id(connectionId).delete()
         print ("Connection name:", connection.name)
 
     async def create_schema(self, connectionId: str):
         schema = Schema(
             base_type = "microsoft.graph.externalItem",
-            properties = [
-               Property_(
-                name= str,
-                type= str,
-                is_searchable= True,
-                is_retrievable= True,
+            properties = [ 
+                Property_(
+                    name= "name",
+                    type=PropertyType.String,
+                    is_searchable= True,
+                    is_retrievable= True,
                 )
             ]
-        )
-        
-        requestInfo = self.user_client.external.connections.by_external_connection_id(connectionId).schema.to_patch_request_information(body=schema)
-        print ("RequestInfo:", requestInfo.content)
-        requestMessage = await self.user_client.request_adapter.convert_to_native_async(requestInfo)
-        requestMessage.http_method = Method.POST
-        print ("RequestMessage:", requestMessage)
-        res = await GraphRequestAdapter.send_async(self, requestMessage, schema, schema)
-        print ("Schema:", res)
-        #try:
-         #   res = await self.user_client.external.connections.by_external_connection_id(connectionId).schema.patch(body=schema)
-          #  print ("Schema:", res)
-        #except Exception as e:
-         #   print (e)
-        #res = await self.user_client.external.connections.by_external_connection_id(connectionId).schema.patch(body=schema,request_configuration=None)
-        #print ("Schema:", res)
+        )  
+        print("Creating schema...")      
+        await self.user_client.external.connections.by_external_connection_id(connectionId).schema.patch(schema)
+        print ("Schema created successfully!")
--- main.py
+++ main.py
@@ -13,7 +13,8 @@ async def main():
 
     graph: Graph = Graph(azure_settings)
 
-    await greet_user(graph)
+    user = await graph.get_user()
+    print('Hello,', user.display_name)
 
     choice = -1
 
@@ -34,69 +35,22 @@ async def main():
             if choice == 0:
                 print('Goodbye...')
             elif choice == 1:
-                await create_external_connection(graph)
+                await graph.create_connection()
             elif choice == 2:
-                await create_schema(graph)
+                connectionId = input("Please enter the connection id: ")
+                await graph.create_schema(connectionId)
             elif choice == 3:
-                await get_external_connection(graph)
+                connectionId = input("Please enter the connection id: ")
+                await graph.get_connection(connectionId)
             elif choice == 4:
-                await list_external_connections(graph)
+                await graph.list_connections()
             elif choice == 5:
-                await delete_external_connection(graph)
+                connectionId = input("Please enter the connection id: ")
+                await graph.delete_connection(connectionId)
             else:
                 print('Invalid choice!\n')
         except Exception as ex:
             print('Error:', ex, '\n')
 
-async def greet_user(graph: Graph):
-    user = await graph.get_user()
-    if user:
-        print('Hello,', user.display_name)
-        # For Work/school accounts, email is in mail property
-        # Personal accounts, email is in userPrincipalName
-        print('Email:', user.mail or user.user_principal_name, '\n')
-
-async def display_access_token(graph: Graph):
-    token = await graph.get_user_token()
-    print('User token:', token, '\n')
-
-async def list_inbox(graph: Graph):
-    message_page = await graph.get_inbox()
-    if message_page and message_page.value:
-        # Output each message's details
-        for message in message_page.value:
-            print('Message:', message.subject)
-            if (
-                message.from_ and
-                message.from_.email_address
-            ):
-                print('  From:', message.from_.email_address.name or 'NONE')
-            else:
-                print('  From: NONE')
-            print('  Status:', 'Read' if message.is_read else 'Unread')
-            print('  Received:', message.received_date_time)
-
-        # If @odata.nextLink is present
-        more_available = message_page.odata_next_link is not None
-        print('\nMore messages available?', more_available, '\n')
-
-async def create_external_connection(graph: Graph):
-    await graph.create_connection()
-
-async def create_schema(graph: Graph):
-    connectionId = input("Please enter the connection id: ")
-    await graph.create_schema(connectionId)
-
-async def get_external_connection(graph: Graph):
-   connectionId = input("Please enter the connection id: ")
-   await graph.get_connection(connectionId)
-
-async def list_external_connections(graph: Graph):
-   await graph.list_connections()
-
-async def delete_external_connection(graph: Graph):
-   connectionId = input("Please enter the connection id: ")
-   await graph.delete_connection(connectionId)
-   
 # Run main
 asyncio.run(main())
